e47a8f3cbf043561f57724cea2884b31b5d1f4fe
alina-koman <alinakoman962@gmail.com>
2026-09-05 16:05:47 +0300

    create user model

--- index.js
+++ index.js
@@ -1,4 +1,10 @@
 import express from 'express'
+import jwt from 'jsonwebtoken'
+import mongoose from 'mongoose'
+
+mongoose.connect('mongodb+srv://alinakoman962_db_user:qwerty123@cluster0.tqz0nej.mongodb.net/?appName=Cluster0')
+    .then(() => console.log('Connected to DB'))
+    .catch(err => console.log(err));
 
 const app = express()
 
@@ -10,9 +16,19 @@ app.get('/', (req, res) => {
 
 app.post('/auth/login', (req, res) => {
     console.log(req.body)
+
+    if (req.body.email === 'test@test.ua') {
+        const token = jwt.sign({
+                email: req.body.email,
+                fullName: 'Вася Пупкін'
+            },
+            'secret123'
+        )
+    }
+
     res.json({
         success: true,
-
+        token
     })
 })
 
